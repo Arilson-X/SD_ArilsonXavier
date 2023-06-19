25295533a21e52106e88e072efffb87a323afc7c
Arilson-X <arilsonouzacosta0@gmail.com>
2023-06-19 11:07:45 -0300

    Adicionado projeto da aula do LabSpa

--- workspace_eclipse/la_rest_server/src/main/java/br/inatel/labs/labsrest/server/controller/ProdutoController.java
+++ workspace_eclipse/la_rest_server/src/main/java/br/inatel/labs/labsrest/server/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 import br.inatel.labs.labsrest.server.model.Produto;
 import br.inatel.labs.labsrest.server.service.ProdutoService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
--- workspace_vscode/lab-spa/src/app/app-routing.module.ts
+++ workspace_vscode/lab-spa/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdutoTabelaComponent } from './components/produto-tabela/produto-tabela.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'produtotabela',
+    component: ProdutoTabelaComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
