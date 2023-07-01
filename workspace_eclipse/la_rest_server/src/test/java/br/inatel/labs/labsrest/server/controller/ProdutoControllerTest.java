6e755ffa31ed65afe12b9080fd03777adc3e7930
Arilson-X <arilsonouzacosta0@gmail.com>
2023-07-01 16:31:40 -0300

    Atualizado os teste de Produto Controller

--- workspace_eclipse/la_rest_server/src/test/java/br/inatel/labs/labsrest/server/controller/ProdutoControllerTest.java
+++ workspace_eclipse/la_rest_server/src/test/java/br/inatel/labs/labsrest/server/controller/ProdutoControllerTest.java
@@ -1,9 +1,14 @@
 package br.inatel.labs.labsrest.server.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import br.inatel.labs.labsrest.server.model.Produto;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProdutoControllerTest {
@@ -23,5 +28,31 @@ class ProdutoControllerTest {
 				.returnResult()
 			;
 	}
-
+	
+	@Test
+	void dadoProdutoIdValido() {
+		Long produtoIdValido = 1L;
+		
+		Produto produtorespondido = webTestClient.get()
+				.uri("produto/"+produtoIdValido)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(Produto.class)
+					.returnResult()
+					.getResponseBody();
+		assertNotNull(produtorespondido);
+		assertEquals(produtorespondido.getId(), produtoIdValido);
+	}
+	
+	@Test
+	void dadoProdutoIdInvalido() {
+		Long idInvalido = 99L;
+		
+		webTestClient
+			.get()
+			.uri("/produto/"+ idInvalido)
+			.exchange()
+			.expectStatus()
+				.isNotFound();
+	}
 }
